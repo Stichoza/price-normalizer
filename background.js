8d8d62f39e3bbeb6e15fbd67ece3fc8cf8c39730
Stichoza <stichoza@gmail.com>
2013-04-18 02:48:50 +0400

    Enabled state toggle

--- background.js
+++ background.js
@@ -1,19 +1,23 @@
+// Initialize localStorage
+if (typeof localStorage.enabled != 'undefined') setEnabled(true);
+if (typeof localStorage.active != 'undefined') setActive(false);
+
 // Turn normalizer on or off
-function setEnable(b) {
+function setEnabled(b) {
 	localStorage["enabled"] = b;
 	var state = (b) ? "on" : "off";
 	chrome.browserAction.setBadgeText({text: state});
 }
 
 // Get normalizer enabled state
-function getEnable() {
+function getEnabled() {
 	return localStorage["enabled"];
 }
 
 // Set active state on or off
 function setActive(b) {
 	localStorage["active"] = b;
-	var bg_color = (b) ? [0, 255, 0, 255] : [22, 22, 22, 255];
+	var bg_color = (b) ? [0, 255, 0, 255] : [100, 100, 100, 255];
 	chrome.browserAction.setBadgeBackgroundColor({color: bg_color});
 }
 
@@ -22,5 +26,9 @@ function getActive() {
 	return localStorage["active"];
 }
 
+// Actions fired on popup click
+function popupOpen() {
+	setEnabled(!getEnabled());
+}
 
-setActive(true);
+chrome.browserAction.onClicked.addListener(popupOpen);
