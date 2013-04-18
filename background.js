efccbc658533b55bcbd067478cc8f6135bd7cdd2
Stichoza <stichoza@gmail.com>
2013-04-19 02:30:15 +0400

    Icons and shit

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 // Initialize localStorage
-if (localStorage["enabled"] == 'undefined' || !getEnabled()) setEnabled(true);
-if (localStorage["active"] == 'undefined' || !getActive()) setActive(false);
+setEnabled(localStorage["enabled"] == 'undefined' || !getEnabled());
+setActive(!(localStorage["active"] == 'undefined' || !getActive()));
 
 // Turn normalizer on or off
 function setEnabled(b) {
 	localStorage["enabled"] = (b) ? "on" : "off";
 	chrome.browserAction.setBadgeText({text: localStorage["enabled"]});
 	if (!b) setActive(false);
+	console.log("Enabled: "+localStorage["enabled"]);
 }
 
 // Get normalizer enabled state
@@ -19,6 +20,7 @@ function setActive(b) {
 	localStorage["active"] = (b) ? "ok" : "no";
 	var bg_color = (b) ? [0, 255, 0, 255] : [100, 100, 100, 255];
 	chrome.browserAction.setBadgeBackgroundColor({color: bg_color});
+	console.log("Active: "+localStorage["active"]);
 }
 
 // Get active state
