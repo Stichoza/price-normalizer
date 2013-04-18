7283b5a0a7db33de980e167dc9c2c8ca714f6bcc
Stichoza <stichoza@gmail.com>
2013-04-18 17:48:49 +0400

    Toggle fix

--- background.js
+++ background.js
@@ -1,29 +1,29 @@
 // Initialize localStorage
-if (typeof localStorage.enabled != 'undefined') setEnabled(true);
-if (typeof localStorage.active != 'undefined') setActive(false);
+if (localStorage["enabled"] == 'undefined' || !getEnabled()) setEnabled(true);
+if (localStorage["active"] == 'undefined' || !getActive()) setActive(false);
 
 // Turn normalizer on or off
 function setEnabled(b) {
-	localStorage["enabled"] = b;
-	var state = (b) ? "on" : "off";
-	chrome.browserAction.setBadgeText({text: state});
+	localStorage["enabled"] = (b) ? "on" : "off";
+	chrome.browserAction.setBadgeText({text: localStorage["enabled"]});
+	if (!b) setActive(false);
 }
 
 // Get normalizer enabled state
 function getEnabled() {
-	return localStorage["enabled"];
+	return (localStorage["enabled"] == "on");
 }
 
 // Set active state on or off
 function setActive(b) {
-	localStorage["active"] = b;
+	localStorage["active"] = (b) ? "ok" : "no";
 	var bg_color = (b) ? [0, 255, 0, 255] : [100, 100, 100, 255];
 	chrome.browserAction.setBadgeBackgroundColor({color: bg_color});
 }
 
 // Get active state
 function getActive() {
-	return localStorage["active"];
+	return (localStorage["active"] == "ok");
 }
 
 // Actions fired on popup click
