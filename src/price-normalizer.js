e7304177257db2dd413ad1ca3ae5a92f3b0e854e
Stichoza <stichoza@gmail.com>
2013-04-21 03:40:51 +0400

    Replacing only nodeValues + GPB Sign
    Accidentally unchecked this this file from previous commit, lol.

--- src/price-normalizer.js
+++ src/price-normalizer.js
@@ -1,30 +1,33 @@
 // Main Regex
-var __prcm_rex__ = new RegExp("([$€₠₡₢₣₤₥₦₧₨₩₪₫₭₮₯₰₱₲₳₴₵₶₷₸₹₺])+([0-9, ]{1,10})+([.]{0,1})+([0-9]{1,5})", "igm");
+var __prcm_rex__ = new RegExp("([$£€₠₡₢₣₤₥₦₧₨₩₪₫₭₮₯₰₱₲₳₴₵₶₷₸₹₺])+"+
+	"([0-9, ]{1,10})+([.]{0,1})+([0-9]{1,5})", "igm");
 
 // Find what to replace
 var __prcm_rpl__ = function (s) {
-    return r = s.replace(/([0-9, ]{1,10})+([.]{0,1})+([0-9]{1,5})/, __prcm_ceil__);
+	return r = s.replace(/([0-9, ]{1,10})+([.]{0,1})+([0-9]{1,5})/, __prcm_ceil__);
 }
 
 // How to replace
 var __prcm_ceil__ = function (s) {
-    s = parseFloat(s.replace(/([, ])/gi, ""), 10);
-    var r = Math.round(s);
-    //return (r<s) ? s : "<span class=\"__prcm__\" title=\"Original price was "+s+"\">"+r+"</span>";
-    return (r<s) ? s : r;
+	s = parseFloat(s.replace(/([, ])/gi, ""), 10);
+	var r = Math.round(s);
+	var rem = s*100%100;
+	// console.log(s+" -> "+r+" -> "+rem+" :: "+__prcm_option__("rest_min"));
+	return (r>s && rem >= __prcm_option__("rest_min")) ? r.toFixed(2) : s.toFixed(2);
 }
 
 // Main loop function
-function __prcm__() {
-	var __prcm_elem__ = document.getElementsByTagName("body")[0];
-	var __prcm_match__ = __prcm_elem__.innerHTML.match(__prcm_rex__);
-	var __prcm_count__ = (__prcm_match__ == null) ? 0 : __prcm_elem__.innerHTML.match(__prcm_rex__).length;
+function __prcm_main__(node) {
+	var __prcm_val__ = node.nodeValue;
+	var __prcm_match__ = __prcm_val__.match(__prcm_rex__);
+	var __prcm_count__ = (__prcm_match__ == null) ? 0 : __prcm_val__.match(__prcm_rex__).length;
 	if (__prcm_count__) {
-	    var __prcm_newsrc__ = __prcm_elem__.innerHTML.replace(__prcm_rex__, __prcm_rpl__);
-	    __prcm_elem__.innerHTML = __prcm_newsrc__;
-	    console.log("price-normalizer: replaced "+__prcm_count__+" entries");
-	} else {
-		console.log("price-normalizer: nothing to do");
+		// Set title with original price on node
+		if (__prcm_val__.length < 10) {
+			node.addAtribute
+		}
+		var __prcm_newsrc__ = __prcm_val__.replace(__prcm_rex__, __prcm_rpl__);
+		node.nodeValue = __prcm_newsrc__;
 	}
 }
 
@@ -33,7 +36,9 @@ var __prcm_interval__; // Global interval variable
 var __prcm_interval_b__ = false; // Inteval status
 chrome.runtime.sendMessage({method: "getEnabled"}, function (response) {
 	if (response.enabled == "on") {
-		__prcm_interval__ = window.setInterval(__prcm__, 2000);
+		__prcm_interval__ = window.setInterval(function () {
+			__prcm_dom_walk__(document.getElementsByTagName("body")[0]);
+		}, 2000);
 		__prcm_interval_b__ = true;
 	}
 });
@@ -44,8 +49,42 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		window.clearInterval(__prcm_interval__);
 		__prcm_interval_b__ = false;
 	} else if (request.method == "startLoop" && !__prcm_interval_b__) {
-		__prcm_interval__ = window.setInterval(__prcm__, 2000);
+		__prcm_interval__ = window.setInterval(function () {
+			__prcm_dom_walk__(document.getElementsByTagName("body")[0]);
+		}, 2000);
 		__prcm_interval_b__ = true;
 	}
 	sendResponse({});
 });
+
+// Get local storage variables from exstension runtime
+function __prcm_option__(n) {
+	var requestMessage = {
+		method: "getOption",
+		option: n
+	};
+	chrome.runtime.sendMessage(requestMessage, function (response) {
+		window.__prcm_var_rest__ = response.value;
+	});
+	return window.__prcm_var_rest__;
+}
+
+// Walk DOM elements
+function __prcm_dom_walk__(node) {
+	var child, next;
+	switch (node.nodeType) {
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while (child) {
+				next = child.nextSibling;
+				__prcm_dom_walk__(child);
+				child = next;
+			}
+			break;
+		case 3: // Text node
+			__prcm_main__(node);
+			break;
+	}
+}
